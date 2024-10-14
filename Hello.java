115c41c3b0ecfaea746d481f1dc8770d94339a5c
Rafal Kennedy <rafaelartur12@gmail.com>
2024-10-14 17:42:57 -0300

    primeiro commit

---
+++ Hello.java
@@ -0,0 +1,18 @@
+class Hello
+{
+    public static void main (String a [])
+  {
+   int num1 = 9;
+   byte by = 127;
+   short sh = 558;
+   long l = 5854l;
+
+   float f = 5.8f;
+   double d = 5.8;
+
+   char c = '8';
+
+   
+   boolean b = true;
+  }
+} 
